ab3e49f19c7de841c63ec9505ca995f2cf0d1bbc
Eidan Rones <31554089+eidanrones@users.noreply.github.com>
2018-08-27 08:52:24 +0100

    Update script.js
    Removed obsolete comments

--- script.js
+++ script.js
@@ -6,7 +6,3 @@ window.addEventListener("resize", function() {
   let vh = window.innerHeight;
   document.getElementById('value').innerHTML = vh;
 })
-
-//var style = getComputedStyle(document.body);
-//console.log(style.getPropertyValue('--realvh'));
-//style.setProperty('--realvh', vh);
